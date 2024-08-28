fa4dac97427c2b9d2c995030e98db91c1206a19a
Gabriel Pereira <gabrieljpe@hotmail.com>
2024-08-28 20:06:19 -0300

    input items test3

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from automation import Automation
 from robocorp.tasks import task
 from robocorp import workitems
-
 @task
 def main():
-    item = workitems.Inputs
-    print(item)
+    item = workitems.inputs.current
+    print(item.payload)
     bot = Automation()
-    bot.run('Test', 1, item)
+    bot.run('Test', 1, item.payload)
 
 if __name__ == "__main__":
     main()
