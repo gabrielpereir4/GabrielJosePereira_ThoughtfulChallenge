4899b6bd62942a7fd92c13baa2e8c718d0f7ea6e
Gabriel Pereira <gabrieljpe@hotmail.com>
2024-08-28 19:33:59 -0300

    input items test

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from automation import Automation
 from robocorp.tasks import task
+from robocorp import workitems
 
 @task
 def main():
+    item = workitems.inputs.current
+    print(item)
     bot = Automation()
     bot.run('Test', 1)
 
